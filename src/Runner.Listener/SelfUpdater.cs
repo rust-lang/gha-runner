b1c8c4b6729a201857a746e2403a2e6c7dabb132
Pietro Albini <pietro@pietroalbini.org>
2020-01-31 11:02:34 +0100

    Runner.Listener: prevent self-updates
    There is surely a better way to do this, but hey, this hack might be
    enough.

--- src/Runner.Listener/SelfUpdater.cs
+++ src/Runner.Listener/SelfUpdater.cs
@@ -68,6 +68,13 @@ namespace GitHub.Runner.Listener
                 Trace.Info($"An update is available.");
                 _updateTrace.Add($"RunnerPlatform: {_targetPackage.Platform}");
 
+                // RUST: disable self-updates
+                var rustAvoidUnreachableCodeError = true;
+                if (rustAvoidUnreachableCodeError) {
+                    Console.WriteLine("RUST: prevented self-update");
+                    return false;
+                }
+
                 // Print console line that warn user not shutdown runner.
                 await UpdateRunnerUpdateStateAsync("Runner update in progress, do not shutdown runner.");
                 await UpdateRunnerUpdateStateAsync($"Downloading {_targetPackage.Version} runner");
